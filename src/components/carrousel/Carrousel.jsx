2700dce3d802c4780f5d04578b9053fe529e3e48
awzyy <waysbdo@gmail.com>
2023-09-15 10:58:33 +0200

    ajout page accomodations

---
+++ src/components/carrousel/Carrousel.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import './carrousel.scss'
+import arrow_back from "../../assets/arrow_back.png"
+import arrow_next from "../../assets/arrow_next.png"
+
+export default function Slider({ imageSlider }) {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const nextSlide = () => {
+    setCurrentIndex((currentIndex + 1) % imageSlider.length)
+  };
+
+const previouSlide = () => {
+setCurrentIndex((currentIndex - 1 + imageSlider.length) % imageSlider.length)
+};
+
+  return (
+ <section style={{backgroundImage : `url(${imageSlider[currentIndex]})`}} className='carrousel'>
+            {imageSlider.length > 1 && 
+            // if the number of images > 1, then show arrows and counter
+                <>
+                    <img 
+                        className='carrousel_arrow_right' 
+                        src={arrow_next} 
+                        alt="slide suivante" 
+                        onClick={nextSlide}
+                    />
+                    <img 
+                    className='carrousel_arrow_left' 
+                        src={arrow_back} 
+                        alt="slide précédente" 
+                        onClick={previouSlide}
+                    />
+                    <p className='count'>{currentIndex + 1} / {imageSlider.length}</p>
+                </>
+            } 
+        </section>
+  );
+}
---
+++ src/pages/accomodation/Accomodation.jsx
@@ -0,0 +1,87 @@
+import './accomodation.scss'
+import Header from "../../components/header/Header"
+import Footer from "../../components/footer/Footer"
+import Collapse from "../../components/collapse/Collapse"
+import datas from "../../datas/datas"
+import { useParams, useNavigate } from "react-router-dom"
+import ActiveStar from "../../assets/star-active.png"
+import InactiveStar from "../../assets/star-inactive.png"
+import { useEffect, useState } from "react"
+import Slider from "../../components/carrousel/Carrousel"
+
+export default function Accomodation() {
+  const [imageSlider, setImageSlider] = useState([]); // State to store carousel images
+  const navigate = useNavigate(); // Hook for the navigation and error page
+  const { id } = useParams(); // Hosting ID retrieval from URL parameters
+
+  // Data filtering to obtain the current hosting based on the ID
+  const dataCurrentAccomodation = datas.find(data => data.id === id);
+
+  // Redirect to error page if the id of an accomodation is not found
+  useEffect(() => {
+    if (!dataCurrentAccomodation) {
+      navigate('/error'); 
+    } else {
+      setImageSlider(dataCurrentAccomodation.pictures);
+    }
+  }, [dataCurrentAccomodation, navigate]);
+
+  // Hosting page rendering
+  if (!dataCurrentAccomodation) {
+    return null;
+  }
+
+  // Retrieve rating and host name
+  const { rating, host } = dataCurrentAccomodation;
+  const name = host.name.split(' ');
+
+  return (
+    <div>
+      <Header />
+      <main className="accomodation">
+        <Slider imageSlider={imageSlider}/>
+        <div className="info-container">
+          <div className="location-container">
+            <div className="accomodation_title">
+              <h1>{dataCurrentAccomodation.title}</h1>
+            </div>
+            <p>{dataCurrentAccomodation.location}</p>
+            <div>
+              {dataCurrentAccomodation.tags.map((tag, index) => {
+                return (
+                  <button className="tag" key={index}>{tag}</button>
+                )
+              })}
+            </div>
+          </div>
+          <div className="host-container">
+            <div className="stars">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <img
+                  key={index}
+                  src={index < rating ? ActiveStar : InactiveStar}
+                  alt="star"/>
+              ))}
+            </div>
+            <div className="accomodation_host">
+              <div className="host-info">
+                <span className="host-lastname">{name[0]}</span>
+                <span className="host-firstname">{name[1]}</span>
+              </div>
+              <img className="picture" src={host.picture} alt="propriétaire de l'appartement" />
+            </div>
+          </div>
+        </div>
+        <div className="accomodation-collapse">
+          <div className="accomodation-collapse_item">
+            <Collapse title={"Description"} content={dataCurrentAccomodation.description}/>
+          </div>
+          <div className="accomodation-collapse_item">
+            <Collapse title={"Equipements"} content={dataCurrentAccomodation.equipments}/>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  )
+}
